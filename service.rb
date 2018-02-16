b0cd35b81db37af4fed0d8f43257cc8969c50157
Kevin Reedy <kevinreedy@gmail.com>
2018-02-16 14:42:19 -0600

    start structure of service

--- service.rb
+++ service.rb
@@ -1,5 +1,38 @@
 require 'sinatra'
+require 'pry'
+
+configure do
+  enable :logging, :dump_errors, :raise_errors
+end
 
 get '/' do
-  'Hello World!'
+  'Hello World! This project takes notifications from Chef Automate about failed
+  Chef Client and Inspec runs, formats them, and sends the result to a HipChat
+  room. See <a href="https://github.com/kevinreedy/automate-hipchat-notifier">
+  https://github.com/kevinreedy/automate-hipchat-notifier</a> for more details.'
+end
+
+post '/' do
+  message = JSON.parse(request.body.read)
+
+  # Write message to file for now for troubleshooting
+  File.write('./tmp/output.txt', JSON.pretty_generate(message))
+
+  if message['username'] == 'Chef_Automate' && message['attachments']
+    puts 'Received webhook test from Chef Automate'
+    halt 200
+  end
+
+  if message['type'] == 'node_failure'
+    puts 'Received node failure'
+    halt 200
+  end
+
+  if message['type'] == 'compliance_failure'
+    puts 'Received compliance failure'
+    halt 200
+  end
+
+  # Didn't find a message we care about, so 404
+  halt 404
 end
